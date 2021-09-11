384561099d156bada7e90fd7350de4531ed23922
Reut  Kishon <reki8611@gmail.com>
2021-09-12 00:16:23 +0300

    page

--- src/utils.js
+++
@@ -1,5 +0,0 @@
-export const id = () => "_" + Math.random().toString(36).substr(2, 9);
-export const randomHexColorCode = () => {
-    let n = (Math.random() * 0xfffff * 1000000).toString(16);
-    return "#" + n.slice(0, 6);
-};
